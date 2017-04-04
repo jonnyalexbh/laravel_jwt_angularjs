fc70de16cfc6b5a069aa46e5c9140e782262381b
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-04-04 16:30:22 -0500

    create yeoman angularjs project

---
+++ angularjs/app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularjsApp
+ * @description
+ * # angularjsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularjsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
